c31279d12a796627628a07441800e9dda41f6845
Alberto Antonio Perez Paternina <ValledupARCulturAPP@gmail.com>
2019-08-12 01:57:39 -0500

    respaldo

--- js/main.js
+++ js/main.js
@@ -12,3 +12,60 @@
 	});
 	
 })(jQuery);
+
+$(document).ready(function () {
+
+	$("#btnGuardarRegistrar").on("click", function () { SaveRegistrar(); });
+
+
+});
+
+
+function SaveRegistrar() {
+	
+	var form_data = new FormData();
+	var formURL = '167.86.106.173:8989/Registrar/InsertRegistrar';
+
+	var Params = { 
+		Cedula: $('#Cedula').val(), 
+		Nombre: $('#name').val(), 
+		Correo: $('#email').val()
+	};
+
+	form_data.append('Parametros', JSON.stringify(Params));
+	$.ajax(
+	{
+		url: formURL,
+		content: "application/json; charset=utf-8",
+		type: "POST",
+		dataType: "json",
+		data: form_data,
+		contentType: false,
+		processData: false,
+		success: function (data) {
+			if (!data.IsError) {
+				swal({
+					title: "¡Gracias!",
+					text: "Te has registrado correctamente",
+					confirmButtonColor: "#66BB6A",
+					type: "success",
+					closeOnConfirm: true,
+					timer: 20000
+				})
+			}
+			else {
+				swal({
+					title: "¡Atención!",
+					text: "Algo salio mal",
+					confirmButtonColor: "#66BB6A",
+					type: "warning"
+				})
+			}
+		},
+		error: function (jqXHR, textStatus, errorThrown) {
+			console.log(errorThrown);
+		}
+	});
+	
+    
+}
