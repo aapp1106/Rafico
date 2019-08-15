a578f8b598af3eeaa403f0523c3bea91319b9a04
Alberto Antonio Perez Paternina <ValledupARCulturAPP@gmail.com>
2019-08-15 16:55:47 -0500

    respaldo

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
+(function($) {
+	"use strict"
+	
+	// Preloader
+	$(window).on('load', function() {
+		$("#preloader").delay(600).fadeOut();
+	});
 
+	// Mobile Toggle Btn
+	$('.navbar-toggle').on('click',function(){
+		$('#header').toggleClass('nav-collapse')
+	});
+	
+})(jQuery);
 
 $(document).ready(function () {
 	Get_Ciudades();
